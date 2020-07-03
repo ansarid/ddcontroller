91f96423ff2f62ea70b24ed5535e4e61a1a2dd6f
Daniyal Ansari <daniyal.s.ansari+github@gmail.com>
2020-07-03 05:39:37 +0000

    trial 3

--- scuttlepy/wheels.py
+++ scuttlepy/wheels.py
@@ -26,7 +26,7 @@ from scuttlepy import encoder                                               # fo
 
 class Wheel:
 
-    def __init__(self, motor_channel, encoder_address, wheel_radius=41, invert_motor=False, invert_encoder=False, KP=0.04, KI=0.015, KD=0):
+    def __init__(self, motor_channel, encoder_address, wheel_radius=41, invert_motor=False, invert_encoder=False, KP=0.025, KI=0.015, KD=0):
 
         self.speed = 0                                                      # (rad/s), use self.speed instead when possible!
         self.radius = wheel_radius                                          # mm

21866ef83bc73d7a71699488215db77807dae634
dmalawey <42623372+dmalawey@users.noreply.github.com>
2020-07-06 12:18:15 +0800

    math corrections

--- scuttlepy/wheels.py
+++ scuttlepy/wheels.py
@@ -99,9 +99,9 @@ class Wheel:
     if -0.222 < duty and duty < 0.222:
         duty = (duty * 3)
     elif duty >= 0.222:
-        duty = (0.666+(0.429*duty-0.222))
+        duty = 0.666 + (0.429*(duty-0.222))
     else:
-        duty = (-0.666+(-0.429*duty+0.222))
+        duty = -0.666 + (-0.429*(duty+0.222))
         ### THIS NEEDS TO BE REFACTORED ###
 
         duty = sorted([-1, duty, 1])[1]                                     # place bounds on the motor commands
@@ -111,9 +111,9 @@ def rescale(duty):  # a temporary function to perform modified sweep test
     if -0.222 < duty and duty < 0.222:
         duty = (duty * 3)
     elif duty >= 0.222:
-        duty = (0.666+(0.429*duty-0.222))
+        duty = 0.666 + (0.429*(duty-0.222))
     else:
-        duty = (-0.666+(-0.429*duty+0.222))
+        duty = -0.666 + (-0.429*(duty+0.222))
     duty = sorted([-1, duty, 1])[1]      # place bounds on the motor commands
     return duty
 

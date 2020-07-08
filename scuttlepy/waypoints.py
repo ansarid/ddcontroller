1dc57dbd4b68e933b0a7cae091e639585f435a4c
dmalawey <42623372+dmalawey@users.noreply.github.com>
2020-07-08 13:46:20 +0800

    call scuttle.setup before waypoint.

--- scuttlepy/waypoints.py
+++ scuttlepy/waypoints.py
@@ -1,21 +1,6 @@
 import time
 from robot import SCUTTLE
 
-# waypoints = [
-#             [ 0.3,    0],
-#             [ 0.3,  0.3],
-#             [ 0.0,  0.3],
-#             [   0,    0],
-#             ]
-
-# waypoints = [
-#             [ 0.3, 0],
-#             [ 0.3, 0.3],
-#             [ 0.6, 0.3],
-#             [ 0.6, 0.6],
-#             [ 0.9, 0.6],
-#             ]
-
 waypoints = [
             [ 0.3, 0],
             [ 0.3, 0.6],
@@ -23,27 +8,14 @@ waypoints = [
             [ 0.9, 0]
             ]
 
-# waypoints = [
-#             [ 0.3, 0],
-#             [ 0.5, 0.5],
-#             [ 1, 0.5],
-#             ]
 
 scuttle = SCUTTLE()
 
-# print(dir(scuttle))
-
-# scuttle.move([0.5, 0.5])
+scuttle.setup() # take the first encoder readings, establish start
 
 for waypoint in waypoints:
     print(waypoint)
     scuttle.move(waypoint)
-
-# for waypoint in waypoints:
-#     print("DRIVING TO POINT", waypoint)
-#     scuttle.move(waypoint)
-#     print("COMPLETED POINT", waypoint)
-#     print("\n")
 
 # startTime = time.time()
 

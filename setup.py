127a796195286abaf7314145c47d30336e511df7
Daniyal Ansari <daniyal.s.ansari+github@gmail.com>
2021-09-02 17:32:05 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -6,20 +6,23 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
+install_requires=['smbus2',
+                  'fastlogging',
+                 ]
+
+if detector.board.BEAGLEBONE_BLUE:
+    install_requires.append('rcpy')
+elif detector.board.any_raspberry_pi_40_pin:
+    install_requires.append('RPi.GPIO')
+elif detector.board.JETSON_NANO:
+    install_requires.append('Jetson.GPIO')
+    
 setuptools.setup(
     name='scuttlepy',
     version='0.0.1',
     description='SCUTTLE Python Library',
     packages=setuptools.find_packages(),
-    install_requires=['spidev',
-                      'smbus2',
-#                      'RTIMULib',
-                      'fastlogging',
-                      'Adafruit-PlatformDetect',
-#                      'rcpy',                       # Need to make this install based on user platform
-#                      'RPi.GPIO',                   # Need to make this install based on user platform
-                      'Jetson.GPIO',                # Need to make this install based on user platform
-                     ],
+    install_requires=install_requires,
     keywords=['scuttle','robot','python'],
     url='https://github.com/ansarid/scuttlepy',
     long_description=long_description,

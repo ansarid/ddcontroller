0ce89bfea9378422da4e43ad53e89c035a47c36f
Daniyal Ansari <daniyal.s.ansari+github@gmail.com>
2021-12-01 00:09:18 -0600

    wtf?

--- scuttlepy/wheels.py
+++ scuttlepy/wheels.py
@@ -142,10 +142,22 @@ if __name__ == "__main__":
         l_wheel = Wheel(1, 0x40, invert_encoder=True)                           # Left Motor  (ch1)
         r_wheel = Wheel(2, 0x41) 	                                            # Right Motor (ch2)
 
-    elif detector.board.RASPBERRY_PI_40_PIN or detector.board.JETSON_NANO:
+    elif detector.board.any_raspberry_pi_40_pin:
 
+<<<<<<< HEAD:scuttlepy/wheels.py
+        l_wheel = Wheel((15,16), 0x43, invert_encoder=True)                     # Left Motor  (ch1)
+        r_wheel = Wheel((11,12), 0x41) 	                                        # Right Motor (ch2)
+
+    elif detector.board.JETSON_NANO:
+
+=======
+>>>>>>> b0697537b1ae2b177ca877246f08c90c955df59e:scuttlepy/scuttlepy/wheels.py
         l_wheel = Wheel((32,29), 0x43, invert_encoder=True)                     # Left Motor  (ch1)
         r_wheel = Wheel((33,31), 0x41) 	                                        # Right Motor (ch2)
+
+    else:
+        print('Unsupported Platform "', detector.board.id, '"!')
+        exit()
 
     try:
         while True:

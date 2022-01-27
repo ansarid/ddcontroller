92c36b8dd5e8301b2f9cc9a1c39c2ffe976ce24d
Daniyal Ansari <daniyal.s.ansari+github@gmail.com>
2022-01-26 23:03:34 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,6 @@ import pip
 import importlib
 import setuptools
 from os import path
-
-def install(package):
-    if hasattr(pip, 'main'):
-        pip.main(['install', package])
-    else:
-        pip._internal.main(['install', package])
-
-if importlib.util.find_spec('Adafruit-PlatformDetect') is not None:
-    install('Adafruit-PlatformDetect')
-
-from adafruit_platformdetect import Detector
-detector = Detector()
-
 # read the contents of your README file
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md')) as f:
@@ -23,13 +10,6 @@ with open(path.join(this_directory, 'README.md')) as f:
 install_requires=['smbus2',
                   'fastlogging',
                  ]
-
-if detector.board.BEAGLEBONE_BLUE:
-    install_requires.append('rcpy')
-elif detector.board.any_raspberry_pi_40_pin:
-    install_requires.append('RPi.GPIO')
-elif detector.board.JETSON_NANO:
-    install_requires.append('Jetson.GPIO')
 
 setuptools.setup(
     name='scuttlepy',

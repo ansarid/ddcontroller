89d171b2b78340b406c7b88000adac9ae6c1f6e6
Daniyal Ansari <daniyal.s.ansari+github@gmail.com>
2022-12-30 10:43:11 -0700

    autodocs

--- docs/conf.py
+++ docs/conf.py
@@ -13,9 +13,7 @@ author = 'Daniyal Ansari'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-#    "sphinx.ext.autodoc",
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']

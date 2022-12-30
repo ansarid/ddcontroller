4043f7042d947fdfea5ed004a567db15fb817bc2
Daniyal Ansari <daniyal.s.ansari+github@gmail.com>
2022-12-30 10:29:27 -0700

    fixed docs?

--- docs/conf.py
+++ docs/conf.py
@@ -23,5 +23,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+#html_theme = 'sphinx_rtd_theme'
+#html_static_path = ['_static']

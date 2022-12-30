14fb5245632b3671ad392fbcd54a285b4f63a956
Daniyal Ansari <daniyal.s.ansari+github@gmail.com>
2022-12-30 10:38:36 -0700

    undid docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -27,8 +27,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 import sphinx_rtd_theme
 
-#html_theme = "sphinx_rtd_theme"
-#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']

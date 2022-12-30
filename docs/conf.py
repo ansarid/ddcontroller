ac2867ce2b5d8ba19ebea6bffea174eef2ade490
Daniyal Ansari <daniyal.s.ansari+github@gmail.com>
2022-12-30 10:33:04 -0700

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,9 @@ author = 'Daniyal Ansari'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc',]
+extensions = [
+    "sphinx.ext.autodoc",
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -25,8 +27,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 import sphinx_rtd_theme
 
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_theme = "sphinx_rtd_theme"
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-#html_theme = 'sphinx_rtd_theme'
-#html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
